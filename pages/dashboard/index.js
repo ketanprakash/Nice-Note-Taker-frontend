9b25e4da7be989a2d8094f969dd74e5fffd5edc8
ketanprakash <ketanprakash001@gmail.com>
2021-05-19 03:38:38 +0530

    going ahead nicely

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,52 +1,63 @@
 const body = document.querySelector("body");
+const logout = document.querySelector(".logout");
+const createNote = document.querySelector(".new-note")
+;
+const apiUrl = "http://localhost:8000";
+const token = localStorage.getItem("jwt");
+logout.addEventListener("click", () => {
+    localStorage.removeItem("jwt");
+    location.href = "/";
+})
+
+createNote.addEventListener("click", () => {
+    location.href = "/pages/CreateNotes/notes.html";
+})
 window.addEventListener('load', () =>{
     body.classList.add('visible');
 });
-
-const carddata = [
-    {heading: "lorem", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel totam commodi eius. Labore ut hic molestiae aliquid perferendis eveniet molestias ipsum cupiditate, libero, possimus corrupti dolorem blanditiis vero porro ad facere dicta omnis! Sint error quasi at, quaerat dolorem accusantium explicabo. Fugit nisi officia possimus ipsam fugiat cupiditate eum dignissimos.", id: 1},
-    {heading: "ipsum", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 2},
-    {heading: "dolor", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 3},
-    {heading: "sit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 4},
-    {heading: "amet", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel totam commodi eius. Labore ut hic molestiae aliquid perferendis eveniet molestias ipsum cupiditate, libero, possimus corrupti dolorem blanditiis vero porro ad facere dicta omnis! Sint error quasi at, quaerat dolorem accusantium explicabo. Fugit nisi officia possimus ipsam fugiat cupiditate eum dignissimos.", id: 5},
-    {heading: "elit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 6}, 
-    {heading: "elit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 7}, 
-    {heading: "elit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 8}, 
-    {heading: "elit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 9}, 
-    {heading: "elit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 10}, 
-    {heading: "elit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 11}, 
-    {heading: "elit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 12}, 
-    {heading: "elit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 13}, 
-    {heading: "elit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 14}, 
-    {heading: "elit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 15}, 
-    {heading: "elit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 16}, 
-    {heading: "elit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 17}, 
-    {heading: "elit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 18}, 
-    {heading: "elit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 19}, 
-    {heading: "elit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 19}, 
-    {heading: "elit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 20}, 
-    {heading: "elit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 21}, 
-    {heading: "elit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 22}, 
-    {heading: "elit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 23}, 
-    {heading: "elit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 24}, 
-]
-
-const cardContainer = document.querySelector(".card-container")
-
-const createNotes = (array) => {
-    array.forEach(element => {
-        const newCard = document.createElement("div")
-        newCard.classList.add("card")
-        newCard.id = element.id
-        const insideHtml = `<div class="card-header">
-        <div class="card-heading">${element.heading}<a href="../UpdateNotes/notes.html?noteId=${newCard.id}" target="_blank"></div>
-        <div class="edit-note"><img src="./edit-note.svg"/></div></a>
-        </div>
-        <div class="card-content">${element.content}
-        </div>
-        </div>`
-        newCard.innerHTML = insideHtml
-        cardContainer.appendChild(newCard)
-    });
+if (token){
+    fetch(`${apiUrl}/notes/getallnotes`,{
+        method: "GET",
+        headers: {
+            "authorization": `${token}`
+        },
+    }).then((res) => res.json()).then((data) => {
+        const carddata = data;
+        console.log(carddata);
+        const cardContainer = document.querySelector(".card-container")
+        const createNotes = (array) => {
+            array.forEach(element => {
+                const newCard = document.createElement("div")
+                newCard.classList.add("card")
+                newCard.id = element.id
+                const insideHtml = `<div class="card-header">
+                <div class="card-heading">${element.heading}<a href="../UpdateNotes/notes.html?noteId=${newCard.id}" target="_blank"></div>
+                <div class="edit-note"><img src="./edit-note.svg"/></div></a>
+                </div>
+                <div class="card-content">${element.content}
+                </div>
+                </div>`
+                newCard.innerHTML = insideHtml
+                cardContainer.appendChild(newCard)
+            });
+        }
+        createNotes(carddata)
+    }).catch((err) => {
+        alert("Error Signing In!!");
+        console.log(err);
+    })
 }
-createNotes(carddata)
+else{
+    alert("Signin again!!!");
+}
+
+// const carddata = [
+//     {heading: "lorem", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel totam commodi eius. Labore ut hic molestiae aliquid perferendis eveniet molestias ipsum cupiditate, libero, possimus corrupti dolorem blanditiis vero porro ad facere dicta omnis! Sint error quasi at, quaerat dolorem accusantium explicabo. Fugit nisi officia possimus ipsam fugiat cupiditate eum dignissimos.", id: 1},
+//     {heading: "ipsum", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 2},
+//     {heading: "dolor", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 3},
+//     {heading: "sit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 4},
+//     {heading: "amet", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel totam commodi eius. Labore ut hic molestiae aliquid perferendis eveniet molestias ipsum cupiditate, libero, possimus corrupti dolorem blanditiis vero porro ad facere dicta omnis! Sint error quasi at, quaerat dolorem accusantium explicabo. Fugit nisi officia possimus ipsam fugiat cupiditate eum dignissimos.", id: 5},
+//     {heading: "elit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 6}, 
+//     {heading: "elit", content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, aliquam.", id: 7}
+// ]
+
--- pages/signin/index.js
+++ pages/signin/index.js
@@ -1,4 +1,69 @@
 const body = document.querySelector("body");
+
+const apiUrl = "http://localhost:8000";
 window.addEventListener('load', () =>{
     body.classList.add('visible');
 });
+
+const signUpForm = document.querySelector(".signup");
+const signInForm = document.querySelector(".signin");
+
+signInForm.addEventListener("submit", () =>{
+    event.preventDefault();
+    console.log("submitted")
+    const email = document.querySelector(".signin-email").value;
+    const password = document.querySelector(".signin-password").value;
+    // console.log(name, email, password, cpassword);
+    fetch(`${apiUrl}/auth/signin`,{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({email, password})}).then((res) => res.json()).then((data) => {
+            console.log(data)
+            const {token} = data; 
+            if (token){
+                localStorage.setItem("jwt", token);
+                location.href = "/pages/dashboard/dashboard.html"
+            }
+            else{
+                alert("signup/signin again");
+            }
+        }).catch((err) => {
+        alert("Error Signing In!!");
+        console.log(err);
+    })
+})
+
+signUpForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log("submitted")
+    const email = document.querySelector(".signup-email").value;
+    const name = document.querySelector(".signup-name").value;
+    const password = document.querySelector(".signup-password").value;
+    const cpassword = document.querySelector(".csignup-password").value;
+    // console.log(name, email, password, cpassword);
+    if (password !== cpassword){
+        alert("passwords don't match");
+        return;
+    }
+    fetch(`${apiUrl}/auth/signup`,{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({name, email, password})}).then((res) => res.json()).then((data) => {
+            console.log(data)
+            const {token} = data; 
+            if (token){
+                localStorage.setItem("jwt", token);
+                location.href = "/pages/dashboard/dashboard.html"
+            }
+            else{
+                alert("signup/signin again");
+            }
+        }).catch((err) => {
+        alert("Error Signin Up!!");
+        console.log(err);
+    })
+})
