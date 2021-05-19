bbb8601e0e0e2bc67df127029a6f1fcc5dced71c
ketanprakash <ketanprakash001@gmail.com>
2021-05-19 19:02:11 +0530

    connected frontend with backend

--- pages/CreateNotes/index.js
+++ pages/CreateNotes/index.js
@@ -1,9 +1,27 @@
 const body = document.querySelector("body");
+const token = localStorage.getItem("jwt");
+const apiUrl = "http://localhost:8000"
 window.addEventListener('load', () =>{
     body.classList.add('visible');
 });
 
-const createNoteInput = document.querySelector(".note")
-createNoteInput.addEventListener("input", (e) =>{
-    console.log(e.target.value);
+const createNoteButton = document.querySelector(".create");
+
+createNoteButton.addEventListener("click", () => {
+    if (token){
+        const createNoteInput = document.querySelector(".note").value;
+        const createNoteHeading = document.querySelector(".note-heading").value;
+        fetch(`${apiUrl}/notes/add`,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "authorization": token
+            },
+            body: JSON.stringify({heading: createNoteHeading, content: createNoteInput})}).then((res) => res.json()).then((data) => {
+                console.log(data);   
+            }).catch((err) => {
+            alert("Error Creating Note!!");
+            console.log(err);
+        })
+    }
 })
--- pages/UpdateNotes/index.js
+++ pages/UpdateNotes/index.js
@@ -1,12 +1,54 @@
+const urlParams = new URLSearchParams(window.location.search);
 const body = document.querySelector("body");
 window.addEventListener('load', () =>{
     body.classList.add('visible');
 });
-
-const createNoteInput = document.querySelector(".note")
-
-const urlParams = new URLSearchParams(window.location.search)
-
+const apiUrl = "http://localhost:8000";
+const token = localStorage.getItem("jwt");
+const createNoteInput = document.querySelector(".note");
+const headingInput = document.querySelector(".note-heading");
+const updateNote = document.querySelector(".create")
 const noteId = urlParams.get("noteId")
+
+if (token){
+    fetch(`${apiUrl}/notes/getallnotes`,{
+        method: "GET",
+        headers: {
+            "authorization": `${token}`
+        },
+    }).then((res) => res.json()).then((data) => {
+        const carddata = data;
+        console.log(carddata);
+        carddata.forEach((element) => {
+            console.log(element.id);
+            if (element.id == noteId){
+                console.log(element.id);
+                headingInput.value = element.heading; 
+                createNoteInput.value =  element.content;
+            }
+        })
+    }).catch((err) => {
+        alert("Error loading note!");
+        console.log(err);
+    })
+}
+
+updateNote.addEventListener("click", () => {
+    if (token){
+        fetch(`${apiUrl}/notes/update/${noteId}`,{
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "authorization": token
+            },
+            body: JSON.stringify({heading: headingInput.value, content: createNoteInput.value})}).then((res) => res.json()).then((data) => {
+                console.log(data);   
+                alert(data.message);
+            }).catch((err) => {
+            alert("Error Updating Note!!");
+            console.log(err);
+        })
+    }
+})
 
 console.log(noteId)
